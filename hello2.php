f22216c9391f94542b1e94ee0c43fdb8517e0fa4
rigikit <78465030+rigikit@users.noreply.github.com>
2021-02-07 13:28:59 +0900

    Add files via upload

---
+++ hello2.php
@@ -0,0 +1,27 @@
+<?php 
+$name="rk";
+
+if($name=="rk"){
+echo "私はあなたの名前です".PHP_EOL;
+} else {
+echo "あなたの名前ではありません";
+}
+for($i=1; $i<10001; $i++){
+  echo $i;
+}
+echo "\n";
+
+$fruits=["apple","banana","lemon","orange","pineapple"];
+foreach($fruits as $a){
+ echo $a.PHP_EOL;}
+/* for文の始めの値を定義する*/
+ $start=1;
+ /* for文の終わりの値を定義する */
+ $end=101;
+
+ for($i=$start; $i<$end; $i++){
+   if($i%5==0){
+     echo $i;
+   }
+ }
+?>
