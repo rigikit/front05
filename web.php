8264895b1bb6171be6331a5b0d852428be46165a
rigikit <78465030+rigikit@users.noreply.github.com>
2021-02-10 20:49:23 +0900

    Add files via upload

---
+++ web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+//課題3
+Route::group(['prefix' => 'XXX'], function() {
+    Route::get('', 'AAAController@bbb');
+});
+
+
+//課題4
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'ProfileController@add');
+    Route::get('profile/edit', 'ProfileController@edit');
+});
